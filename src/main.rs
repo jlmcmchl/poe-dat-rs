f6bbb0bfe0bc1f72ca76003ade53b9fa677e54c7
jlmcmchl <jlmcmchl@gmail.com>
2020-10-01 19:39:48 -0400

    fast

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use glob::glob;
+use glob::{PatternError, glob};
 use std::fs::File;
 use std::io::prelude::*;
+use rayon::prelude::*;
 
 use poe_dat_rs::types;
 
-fn main() -> Result<(), String> {
-    for path in glob("G:\\POE\\Data\\*.dat").unwrap() {
-        let path = path.unwrap();
-        let mut file = File::open(path.as_path()).map_err(|err| format!("{:?}", err))?;
+fn main() -> Result<(), PatternError> {
+    let files: Vec<_> = glob("G:\\POE\\Data\\*.dat")?.filter_map(|x| x.ok()).collect();
+    let results: Vec<_> = files.par_iter().map(|path| {
+        let mut file = File::open(path.as_path()).map_err(|err| format!("{:?}", err)).unwrap();
 
         let mut contents = Vec::<u8>::new();
         let len = file
-            .read_to_end(&mut contents)
-            .map_err(|err| format!("{:?}", err))?;
+            .read_to_end(&mut contents).unwrap();
 
         let fname = path.as_path().file_name().unwrap().to_str().unwrap();
 
@@ -20,7 +20,7 @@ fn main() -> Result<(), String> {
         let parsed = types::PoeData::parse_file(fname, &contents[..]);
 
         println!("{}", parsed.is_ok());
-    }
+    }).collect();
 
     Ok(())
 }
